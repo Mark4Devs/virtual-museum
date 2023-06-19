af6d60b7e50aee5dffe83bddb8556c0ef979a4ba
Mark4Devs <102985129+Mark4Devs@users.noreply.github.com>
2023-06-19 06:51:49 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,21 +3,30 @@ let activateBtn = document.getElementById('activatePanorama'),
     panorama = document.getElementById('panorama');  
 const awards = {
     "title": "Досягнення університету",
+    "title-2": "Юрій Васильович Кондратюк",
     "image-1": "/images/IMG_8139.JPG",
     "image-2": "/images/IMG_8140.JPG",
-    "desc": "Даний розділ експозиції презентує новітні досягнення науково-педагогічного колективу університету за роки незалежності України." 
-} 
-const insitute = {
+    "image-3": "/images/кондратюк.jpg",
+    "image-4": "/images/yuri-kondratyuk.jpg",
+    "desc": "Окрему сторінку експозиції присвячено  нашому видатному краянинові, ученому-винахідникові, розробнику ракетної техніки і теорії космічних польотів  Юрію Васильовичу Кондратюку(Олександру Гнатовичу Шаргею), ім’я якого з 1997 року носить університет."  
+}  
+const insitute = { 
     "titlePerson-1": "П.П. Гулак - Артемовський",
     "imagePerson-1": "/images/p-p-artemovsky.jpg",
+    "desc-person-1": "Понад тридцять років здійснював нагляд за навчальною частиною видатний український байкар, ректор Харківського університету, професор   П. П. Гулак-Артемовський,  п’ять доньок якого – Аполлінарія, Єфросинія, Марія, Наталія і Клеопатра –   з 1854 по 1865 роки навчалися у  Полтавському Інституті шляхетних дівчат.",
     "titlePerson-2": "В.О. Рєпніна",
     "imagePerson-2": "/images/v-o-repnina.jpg",
     "titlePerson-3": "М.Г. Рєпнін",
     "imagePerson-3": "/images/m-g-repnin.jpg",
+    "titlePerson-4": "М.Г. Рєпнін",
+    "imagePerson-4": "/images/m-g-repnin.jpg",
+    "desc-person-4": "56 років присвятив дівочому інституту як викладач фортепіанної гри та італійського співу видатний чеський музичний педагог і композитор Алоїз Єдлічка, відомий збирач та видавець музичного українського фольклору, автор першої музичної редакції п’єси І.П. Котляревського «Наталка-Полтавка». ",
     "sectionTitle": "Інститут шляхетних дівчат ",
     "sectionImage": "/images/section-image.JPG",
-    "sectionDesc-1": "Перший розділ експозиції музею присвячено висвітленню діяльності Інституту шляхетних дівчат – середнього навчального закладу для збіднілих дворянок – та історії будівництва центрального корпусу навчального закладу, що є пам’яткою архітектури першої половини ХІХ ст. ",
-    "sectionDesc-2": "Полтавський  Інститут шляхетних дівчат засновано 12 грудня 1818 року на кошти дворянства з ініціативи дружини Полтавського та Чернігівського генерал-губернатора князя М. Г. Рєпніна – Варвари Олексіївни. ",   
+    "sectionDesc-1": "З історичних джерел відомо, що Полтавський інститут шляхетних дівчат було засновано 12 грудня 1818 року на кошти дворян Полтавської та Чернігівської губерній. Зініціювала створення закладу В.О. Рєпніна, дружина Полтавського генерал-губернатора М.Г. Рєпніна, онука останнього гетьмана Лівобережної України К. Розумовського.",
+    "sectionDesc-2": "Значну роль у вихованні молодих дівчат відіграв вплив найяскравіших викладачів Інституту шляхетних дівчат –  плеяди видатних представників української та світової культури. Науково-освітні й педагогічні традиції було започатковано такими відомими педагогами, як П. П. Гулак-Артемовський, Л. І. Боровиковський, М. М. Цертелєв, І. К.Зайцев, В. О. Волков, А. В.Єдлічка та багато інших.",   
+    "diorama-image": "/images/history-image.jpg",
+    "diorama-title": "У великій діорамі мистецьки виконано макет Інституту шляхетних дівчат,  що відтворює вигляд центрального корпусу навчального закладу початку ХIХ ст."
 }
 const rectors = {
     "rectorsTitle": "Перші ректори університету", 
@@ -43,10 +52,18 @@ lightGallery(document.querySelector('.tabs-images.two'), {
 lightGallery(document.querySelector('.tabs-images.three'), { 
     controls: true,
     selector: '.gallery-item.three' 
-}); 
-lightGallery(document.querySelector('.tabs-images.four'), { 
+});
+lightGallery(document.querySelector('.chapter-item'), { 
     controls: true,
-    selector: '.gallery-item.four' 
+    selector: '.chapter-link'  
+}); 
+lightGallery(document.querySelector('.chapter-item.two'), { 
+    controls: true,
+    selector: '.chapter-link.two' 
+}); 
+lightGallery(document.querySelector('.chapter-item.three'), { 
+    controls: true,
+    selector: '.chapter-link.three' 
 }); 
 pannellum.viewer('panorama', {
     "default": {
@@ -72,11 +89,25 @@ pannellum.viewer('panorama', {
                     "sceneId": "scene-2"
                 }, 
                 {
-                    "pitch": -9, 
-			        "yaw": 220.5,
+                    "pitch": -10.5, 
+			        "yaw": 218,
 			        "cssClass": "custom-hotspot",
 			        "createTooltipFunc": hotspotAwards,
-			        "createTooltipArgs": "<h3>"+awards.title+"</h3><img src='"+awards["image-1"]+"'><img src='"+awards["image-2"]+"'><p>"+awards.desc+"</p>"
+			        "createTooltipArgs": "<h3>"+awards.title+"</h3><img src='"+awards["image-1"]+"'>" 
+                }, 
+                {
+                    "pitch": -10.5, 
+			        "yaw": 227,
+			        "cssClass": "custom-hotspot",
+			        "createTooltipFunc": hotspotAwards,
+			        "createTooltipArgs": "<h3>"+awards.title+"</h3><img src='"+awards["image-2"]+"'>"
+                }, 
+                {
+                    "pitch": 19, 
+			        "yaw": 222,
+			        "cssClass": "custom-hotspot",
+			        "createTooltipFunc": hotspotAwardsPerson,
+			        "createTooltipArgs": "<h3>"+awards["title-2"]+"</h3><img src='"+awards["image-3"]+"' class='person-sm'><img src='"+awards["image-4"]+"' class='person-sm'><p class='person-text'>"+awards.desc+"</p>",
                 }
             ]
         },
@@ -102,7 +133,7 @@ pannellum.viewer('panorama', {
                     "yaw": 12.5, 
                     "cssClass": "custom-hotspot",
                     "createTooltipFunc": hotspotPerson,
-			        "createTooltipArgs": "<img src='"+insitute["imagePerson-1"]+"'><h5>"+insitute["titlePerson-1"]+"</h5>"
+			        "createTooltipArgs": "<img src='"+insitute["imagePerson-1"]+"'><h5>"+insitute["titlePerson-1"]+"</h5><p>"+insitute["desc-person-1"]+"</p>"
                 },
                 {
                     "pitch": 5,
@@ -117,7 +148,7 @@ pannellum.viewer('panorama', {
                     "cssClass": "custom-hotspot",
                     "createTooltipFunc": hotspotPerson,
 			        "createTooltipArgs": "<img src='"+insitute["imagePerson-3"]+"'><h5>"+insitute["titlePerson-3"]+"</h5>"
-                },
+                }, 
                 { 
                     "pitch": -12,
                     "yaw": 6, 
@@ -131,7 +162,14 @@ pannellum.viewer('panorama', {
                     "cssClass": "custom-hotspot", 
                     "createTooltipFunc": hotspotRectors,
 			        "createTooltipArgs": "<h3>"+rectors.rectorsTitle+"</h3><img src='"+rectors.rectorsImage+"'> "
-                }
+                },
+                { 
+                    "pitch": 0,
+                    "yaw": 49, 
+                    "cssClass": "custom-hotspot",
+                    "createTooltipFunc": hotspotInstitute,
+			        "createTooltipArgs": "<img src='"+insitute["diorama-image"]+"'><h3 class='diorama-title'>"+insitute["diorama-title"]+"</h3> "
+                }   
             ]
         }
     }
@@ -150,6 +188,12 @@ pannellum.viewer('panorama', {
 }); 
 function hotspotAwards(hotSpotDiv, args) {
 	hotSpotDiv.classList.add('custom-tooltip');
+	var infoDiv = document.createElement('article');
+	infoDiv.innerHTML = args;
+	hotSpotDiv.appendChild(infoDiv);  
+} 
+function hotspotAwardsPerson(hotSpotDiv, args) {
+	hotSpotDiv.classList.add('custom-tooltip-awards-person');
 	var infoDiv = document.createElement('article');
 	infoDiv.innerHTML = args;
 	hotSpotDiv.appendChild(infoDiv);  
@@ -180,3 +224,4 @@ activateBtn.onclick = () =>{
         panorama.style.zIndex = "900";
     };
 }
+
